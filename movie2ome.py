0450811ddf0f9e34387a962cd5badbf1c2f55f29
ahms50 <132490971+ahms50@users.noreply.github.com>
2025-11-26 17:39:47 +0000

    Update movie2ome.py

--- movie2ome.py
+++ movie2ome.py
@@ -1,5 +1,8 @@
+
 import numpy as np
+import time
 import csv
+import pickle
 import os
 import re
 import struct
@@ -9,12 +12,185 @@ from ome_types.model import OME, Image, Pixels, Channel, Plane
 import sys
 
 
+
+
+################################## CHANGE EVERYTHING IN THIS SECTION ######################
+
+load_previous_positions=1 # 1 to load previous positions you have already selected
+                          # 0 to choose new positions for samples
+
+main_folder='C:/Users/ahm50/Documents/juriscope_trials' # Give the file directory you are saving to
+
+objective=40 # Set either 20 or 40 for which objective is being used
+
+samples=2 #How many wells/capillaries do you have
+
+sample_names=['A_BOH_2u_anch,BOH_2u_anch']
+
+z_stack=1# Say if a z stack is being used; 1=yes, 0= not
+timelapse=1 # say if you want time lapse on; 1=yes, 0= not
+
+auto_focus=1 # Say if you want autofocus ON or OFF
+
+
+z_c_order='zc'  # zc -  In single z-stack, does all channels then moves to next z_stack -- should be faster
+                # cz -  In single c, does all z_stack and moves to next c --- should be more precise
+
+##################################### time range
+
+interval=np.array([15]) # interval time between imaging cycles in minutes
+
+
+interval_time=np.array([20]) #  total time for each interval, same order as interval
+
+interval_time=interval_time*60 # This is for interval_time in hrs.
+                               # If using in minutes, comment out.
+
+## Further intervals can be added
+
+################################### z step range
+z_range=3 # +- this number, e.g. if it is 20, it is -20Âµm to +20 Âµm
+z_step=1 # step size between z range, i.e. if z_range is 20 and z_step is 1, you would have 41 slices
+
+## Temperature and heating time
+
+enclosure_heating=1 # Say if you want enclosure heating on; 1=yes 2=no
+
+temperature_enclosure = 30
+heating_time = "0:0:1" # h:m:s
+
+#### Peltier temperature variables
+
+peltier=1 # say if you want peltier on; 1=yes, 0=not
+
+pelt_start_temp=30
+pelt_end_temp=20
+pelt_step=-1
+
+pelt_initial_heat_time=30 # how long the initial heating of the first temp lasts for before imaging begins in seconds
+
+pelt_return=1 # Set: 0 -> Only goes from pelt_start_temp to pelt_end_temp in pelt_step
+              #      1 -> Goes from pelt_start_temp to pelt_end_temp and goes back to pelt_start_temp in pelt_step
+
+
+pelt_time_temp_threshold=[29,28,27] # Cutoff temperature where the pelt_wait_time moves to the next wait time sequence
+                                    # if only singular heating time interval used, let this be: []
+                                    
+pelt_wait_time=[4200,30,10,5] # wait time for peltier in minutes. 
+                              # SHOULD BE: length should be: len(pelt_time_temp_threshold) +1
+                              # if only singular heating time interval, should only be 1 interval number and not list
+
+
+################################## Illumination 
+
+
+illumination=[0x40,0x20] # change depending on which channel is being used
+illum_expose=[200000, 100000] # change depending on exposure time for each channel
+laser_pwr=[1,1] # change depending on the laser power wanted, between 0 and 1
+
+##Specify illumination setting
+#first: define the illumination channel that you want to use:
+#ch postion 1: BF 475 nm: 0 bit value , Hex value: 0x01
+#ch postion 2: BF 528 nm: 2 bit value, Hex value: 0x02
+#ch postion 3: BF 625 nm: 4 bit value, Hex value: 0x04
+#ch postion 4: BF 655 nm: 8 bit value, Hex value: 0x08
+#ch postion 5: Fluo 385 nm: 16 bit value, Hex value: 0x10
+#ch postion 6: Fluo 475 nm: 32 bit value, Hex value: 0x20  --- error in microscope so turns on both 475 and 528 on the 40x
+#ch postion 7: Fluo 528 nm: 64 bit value, Hex value: 0x40
+#ch postion 8: Fluo 625 nm: 128 bit value, Hex value: 0x80
+
+
+if load_previous_positions==1:
+    #### Use this to load all positions data after an error. Make sure to comment out the bits above
+    with open(f"{main_folder}/position_data.pkl", "rb") as f:
+        number, positions = pickle.load(f)
+        print("Loaded data:", number, positions)
+
+###################################### END OF USER SET PARAMETERS #################################################
+
+
+
+#################################### Initialisation
+
+os.makedirs(main_folder, exist_ok=True)  # creates dircetory if missing
+
+
+movie_output_path=f'{main_folder}/movies'
+
+os.makedirs(movie_output_path, exist_ok=True)  # creates dircetory if missing
+
+#assign cama name
+
+if objective==20:
+    camera_name = "Genicam FLIR Blackfly S BFS-U3-70S7M 0159F410" # 20x
+elif objective==40:
+    camera_name = "Genicam FLIR Blackfly S BFS-U3-70S7M 0159F411" #40x
+
+
+########### Set peltier array
+
+if pelt_return==0:
+    pelt_temp_list=list(range(pelt_start_temp, pelt_end_temp - 1, pelt_step))
+elif pelt_return==1:
+    pelt_up=list(range(pelt_start_temp, pelt_end_temp - 1, pelt_step))
+    pelt_down=list(range( pelt_end_temp, pelt_start_temp - 1, -pelt_step))
+    pelt_temp_list = pelt_up + pelt_down
+
+
+assert len(pelt_wait_time) == len(pelt_time_temp_threshold) + 1 # check if pelt wait time matches threshold
+
+pelt_wait_schedule = []
+for temp in pelt_temp_list:
+    for i, threshold in enumerate(pelt_time_temp_threshold):
+        if temp > threshold:
+            pelt_wait_schedule.append(pelt_wait_time[i])
+            break
+    else:
+        # If no threshold matched (i.e., temp <= all thresholds)
+        pelt_wait_schedule.append(pelt_wait_time[-1])
+        
+########### Name mapping for illumination
+
+# Channel mapping: hex value -> wavelength string
+illumination_map = {
+    0x01: "475 nm (BF)",
+    0x02: "528 nm (BF)",
+    0x04: "625 nm (BF)",
+    0x08: "655 nm (BF)",
+    0x10: "385 nm (Fluo)",
+    0x20: "475 nm (Fluo)",
+    0x40: "528 nm (Fluo)",
+    0x80: "625 nm (Fluo)",
+}
+
+# Channel mapping laser power
+illumination_val = {
+    0x01: "0",
+    0x02: "1",
+    0x04: "2",
+    0x08: "3",
+    0x10: "4",
+    0x20: "5",
+    0x40: "6",
+    0x80: "7",
+}
+
+
+# Translate hex values into wavelengths
+illumination_names = [illumination_map[val] for val in illumination]
+
+
+
+
+
 ######################## Variables to edit
 
-movie_folder_path="C:/Users/ahm50/Documents/temp/movies/27_8_25"
+movie_folder_path=movie_output_path
 
 
-output_path='C:/Users/ahm50/Documents/temp/movies'
+output_path=f'{main_folder}/tiffs'
+
+os.makedirs(output_path, exist_ok=True)  # creates dircetory if missing
 
 ## Used this: illumination=[0x01,0x60,0x40,0x80] # change depending on which channel is being used
 
@@ -113,110 +289,88 @@ def iterate_frames(data):
 ################################## Functions for ome.tiff conversion ####################
 
 
-def write_ome_tiff(stack, timestamp_list, aq_time, output_path, filename, user_channel_colors):
-    """
-    Write a 5D OME-TIFF (T,C,Z,Y,X) with OME-XML channel colors.
 
-    stack: numpy array (T,C,Z,Y,X)
-    timestamp_list: list of length T with Unix timestamps
-    output_path: folder path
-    filename: output filename (without extension)
-    user_channel_colors: dict {channel_index: color_name_or_hex}
-        color_name_or_hex can be a predefined name (e.g., "red") or hex string (e.g., "#FF8800")
-    """
-    T, C, Z, Y, X = stack.shape
+# -----------------------
+# OMEWriter class 
+# -----------------------
+class OMEWriter:
+    def __init__(self, output_path, filename, shape, timestamp_list, user_channel_colors):
+        T, C, Z, Y, X = shape
 
-    # Predefined ARGB colors (0xAARRGGBB)
-    PREDEFINED_COLORS = {
-        "grey":    '#808080FF',
-        "red":     '#FF0000FF',
-        "green":   '#00FF00FF',
-        "blue":    '#0000FFFF',
-        "yellow":  '#FFFF00FF',
-        "cyan":    '#00FFFFFF',
-        "magenta": '#FF00FFFF',
-        "white":   '#FFFFFFFF',
-        "black":   '#000000FF',
-    }
+        PREDEFINED_COLORS = {
+            "grey":    '#808080FF',
+            "red":     '#FF0000FF',
+            "green":   '#00FF00FF',
+            "blue":    '#0000FFFF',
+            "yellow":  '#FFFF00FF',
+            "cyan":    '#00FFFFFF',
+            "magenta": '#FF00FFFF',
+            "white":   '#FFFFFFFF',
+            "black":   '#000000FF',
+        }
 
-    # Convert hex string to ARGB
-    def hex_to_argb(hex_str):
-        hex_str = hex_str.lstrip("#")
-        if len(hex_str) == 6:  # RGB
-            argb = 0xFF000000 | int(hex_str, 16)
-        elif len(hex_str) == 8:  # ARGB
-            argb = int(hex_str, 16)
-        else:
-            raise ValueError(f"Invalid hex color: {hex_str}")
-        
-        # Convert to signed 32-bit
-        if argb > 0x7FFFFFFF:
-            argb -= 0x100000000
-        
-        return argb
+        def hex_to_argb(hex_str):
+            hex_str = hex_str.lstrip("#")
+            if len(hex_str) == 6:
+                argb = 0xFF000000 | int(hex_str, 16)
+            elif len(hex_str) == 8:
+                argb = int(hex_str, 16)
+            else:
+                raise ValueError("Invalid color")
+            return argb if argb < (1 << 31) else argb - (1 << 32)
 
-    # Build OME Channels
-    channels = []
-    for i in range(C):
-        color_input = user_channel_colors.get(i)
-        if isinstance(color_input, str):
-            argb_value = hex_to_argb(color_input) if color_input.startswith("#") else hex_to_argb(PREDEFINED_COLORS.get(color_input))
-        else:
-            argb_value = 0xFFFFFFFF  # default white
+        # Build OME Channels
+        channels = []
+        for i in range(C):
+            col = user_channel_colors.get(i, "white")
+            if not col.startswith("#"):
+                col = PREDEFINED_COLORS[col]
+            col = hex_to_argb(col)
+            channels.append(Channel(id=f"Channel:{i}", name=f"C{i}", samples_per_pixel=1, color=col))
 
-        channels.append(
-            Channel(
-                id=f"Channel:{i}",
-                name=f"C{i}",
-                samples_per_pixel=1,
-                color=argb_value,
-                #excitation_wavelength=ex, # do properly when you have the correct data
-            )
+        # Build Planes
+        planes = []
+        for t in range(T):
+            for c in range(C):
+                for z in range(Z):
+                    planes.append(
+                        Plane(
+                            the_t=t,
+                            the_c=c,
+                            the_z=z,
+                            delta_t=float(timestamp_list[t])
+                        )
+                    )
+
+        pixels = Pixels(
+            dimension_order="XYZCT",
+            type="uint16",
+            size_x=X, size_y=Y,
+            size_z=Z,
+            size_c=C,
+            size_t=T,
+            channels=channels,
+            planes=planes
         )
 
-    # Build OME Planes
-    planes = [
-    Plane(
-            the_c=cm,
-            the_z=zm,
-            the_t=tm,
-            delta_t=float(timestamp_list[tm]),
-            #acquisition_time=datetime.fromtimestamp(aq_time[tm,cm,zm], tz=timezone.utc).isoformat()
-        )
-        for tm in range(T)   # outermost (slowest)
-        for cm in range(C)   # middle
-        for zm in range(Z)   # innermost (fastest)
-    ]
+        ome = OME(images=[Image(id="Image:0", name=filename, pixels=pixels)])
+        self.ome_xml = ome.to_xml()
 
-    # Create Pixels and OME object
-    pixels = Pixels(
-        dimension_order="XYZCT",
-        type=str(stack.dtype.name),
-        size_x=X,
-        size_y=Y,
-        size_z=Z,
-        size_c=C,
-        size_t=T,
-        channels=channels,
-        planes=planes
-    )
+        self.tif = TiffWriter(f"{output_path}/{filename}.ome.tif", bigtiff=True)
+        self.is_first = True
 
-    ome = OME(images=[Image(id="Image:0", name="Sample Image", pixels=pixels)]) # need to change 'id' and 'name'
-    ome_xml = ome.to_xml()
-
-    # Write TIFF (no colormap needed for 5D stacks)
-    with TiffWriter(f"{output_path}/{filename}.ome.tif", bigtiff=True) as tif:
-        tif.write(
-            stack,
-            description=ome_xml,
+    def write_plane(self, arr):
+        """Write one (Y,X) plane."""
+        self.tif.write(
+            arr,
             compression="lzw",
-            photometric='minisblack'
+            photometric="minisblack",
+            description=self.ome_xml if self.is_first else None
         )
-#################################### code '########################################
+        self.is_first = False
 
-
-
-
+    def close(self):
+        self.tif.close()
 
 
 # List all files in the folder
@@ -228,15 +382,50 @@ files = [f for f in os.listdir(movie_folder_path) if os.path.isfile(os.path.join
 # Dictionary to store files grouped by identifier
 grouped_files = {}
 
-for file in files:
-    if file.startswith("sample_") and "_enclosure" in file:
-        # Extract the identifier between 'sample_' and '_enclosure'
-        identifier = file.split("sample_")[1].split("_enclosure")[0]
+if enclosure_heating==1:
+
+    for file in files: #read every single files
+        if file.startswith("sample_") and "_enclosure" in file:
+            # Extract the identifier between 'sample_' and '_enclosure'
+            identifier = file.split("sample_")[1].split("_enclosure")[0]
+            
+            if identifier not in grouped_files:
+                grouped_files[identifier] = []
+            
+            grouped_files[identifier].append(file)
+
+        # print(grouped_files)
+else:
+    if peltier==1:
+        for file in files: #read every single files
+            if file.startswith("sample_") and "_peltier" in file:
+                # Extract the identifier between 'sample_' and '_enclosure'
+                identifier = file.split("sample_")[1].split("_peltier")[0]
+                
+                if identifier not in grouped_files:
+                    grouped_files[identifier] = []
+                
+                grouped_files[identifier].append(file)
+
+            # print(grouped_files)
+    
+    
+    
+    else:
+    
+    
+        for file in files: #read every single files
+            if file.startswith("sample_") and "." in file:
+                # Extract the identifier between 'sample_' and '_enclosure'
+                identifier = file.split("sample_")[1].split(".")[0]
+                
+                if identifier not in grouped_files:
+                    grouped_files[identifier] = []
+                
+                grouped_files[identifier].append(file)
+
+            # print(grouped_files)
         
-        if identifier not in grouped_files:
-            grouped_files[identifier] = []
-        
-        grouped_files[identifier].append(file)
 
 
 ############ Sort each group in increasing timestamp output is grouped_files
@@ -265,7 +454,7 @@ all_data = []  # This will hold multiple instances of data
 for sample_idx in range(len(sample_names)):
      
      # Open the CSV file
-    with open(f"{movie_folder_path}/frame_mapping_{sample_idx}.csv", mode='r', newline='') as file:
+    with open(f"{main_folder}/frame_mapping_{sample_idx}.csv", mode='r', newline='') as file:
         reader = csv.DictReader(file)
         data = [row for row in reader]  # Each row is a dictionary
         all_data.append(data)  # Store this instance of data
@@ -276,6 +465,7 @@ for sample_idx in range(len(sample_names)):
 
 #################### iterate through frames 
 
+
 for sample_idx in range(len(sample_names)):
 
     num_pos = sorted(set(d.get('number') for d in all_data[sample_idx] if 'number' in d)) # find number of positions per sample
@@ -283,22 +473,32 @@ for sample_idx in range(len(sample_names)):
     for num in num_pos:  # for number of samples in the movie files
 
         num_dicts = [d for d in all_data[sample_idx] if d.get('number')==num] # takes only a specific number from the sample
-        #print(num_dicts)
         c_per_num=sorted(set(d.get('illum_wavelength') for d in num_dicts if 'illum_wavelength' in d)) # find number of channels per sample
+
+        Y, X = 2200, 3208
 
         total_t=len(grouped_files[next(iter(grouped_files))])
         total_c=len(set(d.get('illum_wavelength') for d in num_dicts if 'illum_wavelength' in d))
         total_z=len(set(d.get('z') for d in num_dicts if 'z' in d))
-        stack = np.zeros((total_t, total_c, total_z, 2200, 3208), dtype=np.uint16 )
 
 
         timestamp_list = []
 
-        aq_time = np.zeros((total_t,total_c,total_z), dtype=np.int32)
+        for ti, file in enumerate(grouped_files[sample_names[sample_idx]]):
+            # Extract timestamp from filename
+            timestamp_list.append(int(re.search(r'_timestamp_(\d+)_', file).group(1)))
 
-        
-        
-        
+
+        # --- Create writer ---
+        writer = OMEWriter(
+            output_path=output_path,
+            filename=f"sample_{sample_names[sample_idx]}_position_{num}",
+            shape=(total_t, total_c, total_z, Y, X),
+            timestamp_list=timestamp_list,
+            user_channel_colors=user_channel_colors
+        )
+     
+              
 
         for ti, file in enumerate(grouped_files[sample_names[sample_idx]]): # takes the ordered timelapse for each sample and reads them in order of timelapse T
             movie_name=f"{movie_folder_path}/{file}"
@@ -351,21 +551,17 @@ for sample_idx in range(len(sample_names)):
                             row = row.byteswap()
                         arr[r] = row
                     
-                    frame_time=hdr["time_sec"] + hdr["time_nsec"]/1e9
+                    #frame_time=hdr["time_sec"] + hdr["time_nsec"]/1e9
+                    #iso_time = datetime.fromtimestamp(frame_time, tz=timezone.utc).isoformat().replace("+00:00", "Z")
+
+                    #delta_t = timestamp_list[ti]
 
 
 
-                    aq_time[ti,ci,zi]=frame_time
-                    stack[ti, ci, zi,:,:] = arr
+
+                    writer.write_plane(arr)
+        writer.close()
                     
-        #stack2 = np.transpose(stack, (4, 3, 2, 1, 0))
 
-        write_ome_tiff(
-            stack=stack,
-            timestamp_list=timestamp_list,
-            aq_time=aq_time,
-            output_path=output_path,
-            filename=f"sample_{sample_names[sample_idx]}_position_{num}",
-            user_channel_colors=user_channel_colors
-            
-        )
+print('Conversion to tiff complete')
+
